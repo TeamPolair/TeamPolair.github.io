33a3718b49cc90ee73e207a398bf94459dd2d35f
TeamPolair <72525092+TeamPolair@users.noreply.github.com>
2020-12-03 11:17:20 -0700

    Update topnavScroll.js

--- assets/js/topnavScroll.js
+++ assets/js/topnavScroll.js
@@ -2,9 +2,6 @@ $(function () {
   $(document).scroll(function () {
 	  var $nav = $(".topnav");
 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-	});
-  $(document).scroll(function () {
-	  var $nav = $(".topnav");
-	  $nav.toggleClass('unscrolled', $(this).scrollTop() == $nav.height());
+	  $nav.toggleClass('unscrolled', $(this).scrollTop() <= $nav.height());
 	});
 });
